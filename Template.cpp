e37ec80662e2ab63f45a6e37221c863bc03bc790
KimDoubleB <tree9295@gmail.com>
2020-03-23 23:33:39 +0900

    Add template code

---
+++ Template.cpp
@@ -0,0 +1,13 @@
+// ctrl + alt + C : build
+// ctrl + alt + R : execute
+
+#include <bits/stdc++.h>
+#define bb ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+using namespace std;
+int main() {
+	bb;
+
+	// Problem Solving
+
+	return 0;
+}
